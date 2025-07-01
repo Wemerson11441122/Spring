82b75e4a0037058571bb3f96fb71b019e5da6499
Wemerson <wemersonemao@gmail.com>
2025-07-01 20:16:42 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/wemerson/springjava/SpringJavaApplication.java
+++ src/main/java/com/wemerson/springjava/SpringJavaApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.wemerson.springjava.domain.Categoria;
+import com.wemerson.springjava.domain.Produto;
 import com.wemerson.springjava.repositories.CategoriaRepository;
+import com.wemerson.springjava.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SpringJavaApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJavaApplication.class, args);
@@ -26,7 +31,19 @@ public class SpringJavaApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
