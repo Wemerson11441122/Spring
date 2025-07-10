00cf47abcea81da8c9a931c4685d2aba91ab2efd
Wemerson <wemersonemao@gmail.com>
2025-07-10 20:46:20 -0300

    Ajustes nos endpoint /categorias/id

---
+++ src/main/java/com/wemerson/springjava/resources/exception/ResourseExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.wemerson.springjava.resources.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.wemerson.springjava.services.exceptions.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourseExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+	}
+	
+	
+}
